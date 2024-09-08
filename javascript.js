4e72a73d24dd570b0c1265df6a035b5a537218a0
krish <krishmaniyar27@gmail.com>
2024-09-08 10:10:30 +0530

    index.html

---
+++ javascript.js
@@ -0,0 +1,74 @@
+const arr = ["Rock", "Paper", "Scissor"];
+
+var wins = 0;
+var losses = 0;
+var ties = 0;
+
+function random(mn, mx) {
+    return Math.random() * (mx - mn) + mn;
+}
+
+function GFG_Fun() {
+    return arr[(Math.floor(random(1, 4))) - 1];
+}
+
+let computer = GFG_Fun();
+
+var rock = document.getElementsByClassName("Rock")[0];
+var paper = document.getElementsByClassName("Paper")[0];
+var scissor = document.getElementsByClassName("Scissor")[0];
+
+document.getElementsByClassName("Rock")[0].addEventListener("click", function () {
+    evaluate("Rock");
+});
+document.getElementsByClassName("Paper")[0].addEventListener("click", function () {
+    evaluate("Paper");
+});
+document.getElementsByClassName("Scissor")[0].addEventListener("click", function () {
+    evaluate("Scissor");
+});
+
+function evaluate(user) {
+    if (user == "Rock") {
+        if (computer == "Rock") {
+            ties++;
+        }
+        else if (computer == "Paper") {
+            losses++;
+        }
+        else {
+            wins++;
+        }
+    }
+    else if (user == "Paper") {
+        if (computer == "Rock") {
+            wins++;
+        }
+        else if (computer == "Paper") {
+            ties++;
+        }
+        else {
+            losses++;
+        }
+    }
+    else {
+        if (computer == "Rock") {
+            losses++;
+        }
+        else if (computer == "Paper") {
+            wins++;
+        }
+        else {
+            ties++;
+        }
+    }
+    console.log(computer);
+    console.log(user);
+    console.log(wins + losses + ties)
+    document.getElementById("demo1").innerHTML ="Computer chooses " + computer;
+    document.getElementById("demo2").innerHTML ="Wins: " + wins + " Losses: " + losses + " Ties: " + ties;
+    computer = GFG_Fun();
+}
+
+
+
